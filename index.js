a05fe4a98a68a2707274ecd4dda21be36c658fc7
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-10-17 11:41:12 +0300

    user login & morgan log

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const scrapSeats = require('./scraper')
 
 const app = express()
@@ -8,12 +9,18 @@ app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
 
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(morgan(':remote-addr - :remote-user [:date[web]] ":method :url HTTP/:http-version" :status :res[content-length]'))
 
 app.get('/', async (req, res) => {
-  const seats = await scrapSeats('https://gymservices.yava.gr/login')
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+  const seats = await scrapSeats('https://gymservices.yava.gr/login', user)
   if (seats.length !== 0) {
-    res.json(seats)
+    res.status(200).json(seats)
   } else {
     res.status(500).send({ error: 'Gym is closed' })
   }
