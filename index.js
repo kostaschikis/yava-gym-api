9f5744b7d996abf4f967e2a68703d0b67ae55e14
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-10-17 18:03:14 +0300

    heroku deployment v1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const scrapSeats = require('./scraper')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
